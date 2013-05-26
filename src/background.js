b7ef67d8359370493b6c5b658abfb2f68112b1f9
ito.shion <frontier.engine@gmail.com>
2013-05-26 10:25:09 +0900

    Add context menu to copy tab's title only.

--- src/background.js
+++ src/background.js
@@ -9,25 +9,37 @@ function copyTextToClipboard(text) {
   document.body.removeChild(copyFrom);
 }
 
-var copySimpleTextToClipboard = function(selectedTabs) {
+var copySimpleTextToClipboard = function(selectedTabs, formatFunc) {
   var output = "";
 
   // format output
   for (var i = 0; i < selectedTabs.length; i++) {
     var tab = selectedTabs[i];
-    
-    var title = tab.title;
-    var url = tab.url;
-    var item = title + "\n" + url;
+
+    var item = "";
+    if (formatFunc == null) {
+      item = FormatTitleAndURL(tab);
+    } else {
+      item = formatFunc(tab);
+    }
 
     if (0 < output.length) {
       output += "\n\n";
     }
     output += item;
   }
-  
+
   // do action
   copyTextToClipboard(output);
+}
+
+// formatFunctions
+function FormatTitleAndURL(tab) {
+  return tab.title + "\n" + tab.url;
+}
+
+function FormatTitleOnly(tab) {
+  return tab.title;
 }
 
 // Context Menu
@@ -40,3 +52,14 @@ chrome.contextMenus.create({
     copySimpleTextToClipboard([tab]);
   }
 }, function() { console.log(chrome.extension.lastError);});
+
+chrome.contextMenus.create({
+  "title": "Copy this page's Title",
+  "type": "normal",
+  "id": "copyThisPageTitle",
+  "contexts": ["all"],
+  "onclick": function(info, tab) {
+    copySimpleTextToClipboard([tab], FormatTitleOnly);
+  }
+}, function() { console.log(chrome.extension.lastError);});
+
